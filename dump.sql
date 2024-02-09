d2c1b4ac52e9661cbfa3ac384e4edf737c8b69ad
eduardo-felix <duduflj@gmail.com>
2024-02-09 12:30:48 -0300

    configurações e testes finais

--- dump.sql
+++ dump.sql
@@ -3,6 +3,6 @@ create database nuneSport;
 create table Produtos (
 	id serial primary key,
   	nome text not null,
-  	descricao text not null unique,
+  	descricao text not null,
   	preco numeric not null
 );
